c00fff7b0b1f9c00d2f351104973b0af1b03f32c
Anthony Gargiulo <anthony@agargiulo.com>
2013-01-15 17:18:39 -0500

    Added Fedora 18 to the menu and fetch scripts

--- Fedora/get_fedora.zsh
+++ Fedora/get_fedora.zsh
@@ -17,3 +17,8 @@ wget -nc -O initrd_17_32.img "http://mirror.rit.edu/fedora/linux/releases/17/Fed
 wget -nc -O initrd_17_64.img "http://mirror.rit.edu/fedora/linux/releases/17/Fedora/x86_64/os/images/pxeboot/initrd.img"
 wget -nc -O vmlinuz_17_32 "http://mirror.rit.edu/fedora/linux/releases/17/Fedora/i386/os/images/pxeboot/vmlinuz"
 wget -nc -O vmlinuz_17_64 "http://mirror.rit.edu/fedora/linux/releases/17/Fedora/x86_64/os/images/pxeboot/vmlinuz"
+
+wget -nc -O initrd_18_32.img "http://mirror.rit.edu/fedora/linux/releases/18/Fedora/i386/os/images/pxeboot/initrd.img"
+wget -nc -O initrd_18_64.img "http://mirror.rit.edu/fedora/linux/releases/18/Fedora/x86_64/os/images/pxeboot/initrd.img"
+wget -nc -O vmlinuz_18_32 "http://mirror.rit.edu/fedora/linux/releases/18/Fedora/i386/os/images/pxeboot/vmlinuz"
+wget -nc -O vmlinuz_18_64 "http://mirror.rit.edu/fedora/linux/releases/18/Fedora/x86_64/os/images/pxeboot/vmlinuz"
