b163b291f1597702a709e4197bc1f3a6b62043a5
Anthony Gargiulo <anthony@agargiulo.com>
2013-04-05 00:47:41 -0400

    Silly vim, that's a zsh file not a conf file

--- updates.zsh
+++ updates.zsh
@@ -1,4 +1,4 @@
-# !/bin/zsh
+#!/usr/bin/zsh
 cd CentOS && ./get_centos.zsh
 cd ../
 cd Scientific && ./get_sl.zsh
@@ -9,3 +9,5 @@ cd OpenSUSE && ./get_opensuse.zsh
 cd ../
 echo "Will get Debian working with this script at somepoint.
 For now, everything you need is in the Debian dir"
+
+# vim: set ft=zsh :
