f4a00f7faf640ac59bed296100cb9c07e1c1e0ba
Ryan Buzzell <ryanhypenlee@gmail.com>
2014-09-03 10:15:46 -0400

    Added a force function to allow recreating boot images

--- CentOS/get_centos.zsh
+++ CentOS/get_centos.zsh
@@ -1,41 +1,63 @@
 #!/bin/zsh
-if [[ `pwd` != "/tftpboot/CentOS"  ]] ; then
-	cd /tftpboot/CentOS/
+DIRECTORY="/tftpboot/CentOS"
+
+#Functions
+getall() {
+     wget -O $DIRECTORY/initrd_6_32.img "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/initrd.img"
+     wget -O $DIRECTORY/initrd_6_64.img "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/initrd.img"
+     wget -O $DIRECTORY/initrd_7_32.img "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/initrd.img"
+     wget -O $DIRECTORY/initrd_7_64.img "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/initrd.img"
+     wget -O $DIRECTORY/vmlinuz_6_32 "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/vmlinuz"
+     wget -O $DIRECTORY/vmlinuz_6_64 "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/vmlinuz"
+     wget -O $DIRECTORY/vmlinuz_7_32 "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/vmlinuz"
+     wget -O $DIRECTORY/vmlinuz_7_64 "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/vmlinuz"
+     return 0
+}
+if [[ $1 = "-f" ]]; then
+    getall
 fi
 
-# if [ '$@' == '-f' ]; then
-#     wget -O initrd_6_32.img "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/initrd.img"
-#     wget -O initrd_6_64.img "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/initrd.img"
-#     wget -O initrd_7_32.img "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/initrd.img"
-#     wget -O initrd_7_64.img "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/initrd.img"
-#     wget -O vmlinuz_6_32 "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/vmlinuz"
-#     wget -O vmlinuz_6_64 "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/vmlinuz"
-#     wget -O vmlinuz_7_32 "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/vmlinuz"
-#     wget -O vmlinuz_7_64 "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/vmlinuz"
-#     return 0
-# fi
 
-if [ ! -f /tftpboot/CentOS/initrd_6_32.img ]; then
-    wget -O initrd_6_32.img "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/initrd.img"
+#  for i; do
+#      case "$i" in
+#          -f  ) getall ;;
+# 
+#          --  ) break ;;
+#      esac
+#  done
+
+if [ ! -f $DIRECTORY/initrd_6_32.img ]; then
+    wget -O $DIRECTORY/initrd_6_32.img "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/initrd.img"
 fi
-if [ ! -f /tftpboot/CentOS/initrd_6_64.img ]; then
-    wget -O initrd_6_64.img "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/initrd.img"
+if [ ! -f $DIRECTORY/initrd_6_64.img ]; then
+    wget -O $DIRECTORY/initrd_6_64.img "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/initrd.img"
 fi
-if [ ! -f /tftpboot/CentOS/initrd_7_32.img ]; then
-    wget -O initrd_7_32.img "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/initrd.img"
+if [ ! -f $DIRECTORY/initrd_7_32.img ]; then
+    wget -O $DIRECTORY/initrd_7_32.img "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/initrd.img"
 fi
-if [ ! -f /tftpboot/CentOS/initrd_7_64.img ]; then
-    wget -O initrd_7_64.img "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/initrd.img"
+if [ ! -f $DIRECTORY/initrd_7_64.img ]; then 
+    wget -O $DIRECTORY/vmlinuz_6_32 "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/vmlinuz"
 fi
-if [ ! -f /tftpboot/CentOS/vmlinuz_6_32 ]; then
-    wget -O vmlinuz_6_32 "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/vmlinuz"
+if [ ! -f $DIRECTORY/vmlinuz_6_64 ]; then
+    wget -O $DIRECTORY/vmlinuz_6_64 "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/vmlinuz"
 fi
-if [ ! -f /tftpboot/CentOS/vmlinuz_6_64 ]; then
-    wget -O vmlinuz_6_64 "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/vmlinuz"
+if [ ! -f $DIRECTORY/vmlinuz_7_32 ]; then
+    wget -O $DIRECTORY/vmlinuz_7_32 "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/vmlinuz"
 fi
-if [ ! -f /tftpboot/CentOS/vmlinuz_7_32 ]; then
-    wget -O vmlinuz_7_32 "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/vmlinuz"
+if [ ! -f $DIRECTORY/vmlinuz_7_64 ]; then
+    wget -O $DIRECTORY/vmlinuz_7_64 "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/vmlinuz"
 fi
-if [ ! -f /tftpboot/CentOS/vmlinuz_7_64 ]; then
-    wget -O vmlinuz_7_64 "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/vmlinuz"
-fi
+
+
+#Functions
+getall() {
+     wget -O $DIRECTORY/initrd_6_32.img "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/initrd.img"
+     wget -O $DIRECTORY/initrd_6_64.img "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/initrd.img"
+     wget -O $DIRECTORY/initrd_7_32.img "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/initrd.img"
+     wget -O $DIRECTORY/initrd_7_64.img "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/initrd.img"
+     wget -O $DIRECTORY/vmlinuz_6_32 "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/vmlinuz"
+     wget -O $DIRECTORY/vmlinuz_6_64 "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/vmlinuz"
+     wget -O $DIRECTORY/vmlinuz_7_32 "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/vmlinuz"
+     wget -O $DIRECTORY/vmlinuz_7_64 "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/vmlinuz"
+     return 0
+}
