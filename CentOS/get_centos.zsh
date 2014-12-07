047653987e8972dfcb75024b2b7fc39f2cfcb737
Ryan Buzzell <ryanhypenlee@gmail.com>
2014-12-06 20:21:42 -0500

    Removed doubled block of code. Removed CentOS 32-bit. Closes #2

--- CentOS/get_centos.zsh
+++ CentOS/get_centos.zsh
@@ -5,11 +5,9 @@ DIRECTORY="/tftpboot/CentOS"
 getall() {
      wget -O $DIRECTORY/initrd_6_32.img "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/initrd.img"
      wget -O $DIRECTORY/initrd_6_64.img "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/initrd.img"
-     wget -O $DIRECTORY/initrd_7_32.img "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/initrd.img"
      wget -O $DIRECTORY/initrd_7_64.img "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/initrd.img"
      wget -O $DIRECTORY/vmlinuz_6_32 "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/vmlinuz"
      wget -O $DIRECTORY/vmlinuz_6_64 "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/vmlinuz"
-     wget -O $DIRECTORY/vmlinuz_7_32 "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/vmlinuz"
      wget -O $DIRECTORY/vmlinuz_7_64 "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/vmlinuz"
      return 0
 }
@@ -32,32 +30,12 @@ fi
 if [ ! -f $DIRECTORY/initrd_6_64.img ]; then
     wget -O $DIRECTORY/initrd_6_64.img "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/initrd.img"
 fi
-if [ ! -f $DIRECTORY/initrd_7_32.img ]; then
-    wget -O $DIRECTORY/initrd_7_32.img "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/initrd.img"
-fi
 if [ ! -f $DIRECTORY/initrd_7_64.img ]; then 
     wget -O $DIRECTORY/vmlinuz_6_32 "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/vmlinuz"
 fi
 if [ ! -f $DIRECTORY/vmlinuz_6_64 ]; then
     wget -O $DIRECTORY/vmlinuz_6_64 "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/vmlinuz"
 fi
-if [ ! -f $DIRECTORY/vmlinuz_7_32 ]; then
-    wget -O $DIRECTORY/vmlinuz_7_32 "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/vmlinuz"
-fi
 if [ ! -f $DIRECTORY/vmlinuz_7_64 ]; then
     wget -O $DIRECTORY/vmlinuz_7_64 "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/vmlinuz"
 fi
-
-
-#Functions
-getall() {
-     wget -O $DIRECTORY/initrd_6_32.img "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/initrd.img"
-     wget -O $DIRECTORY/initrd_6_64.img "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/initrd.img"
-     wget -O $DIRECTORY/initrd_7_32.img "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/initrd.img"
-     wget -O $DIRECTORY/initrd_7_64.img "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/initrd.img"
-     wget -O $DIRECTORY/vmlinuz_6_32 "http://mirrors.rit.edu/centos/6.5/os/i386/images/pxeboot/vmlinuz"
-     wget -O $DIRECTORY/vmlinuz_6_64 "http://mirrors.rit.edu/centos/6.5/os/x86_64/images/pxeboot/vmlinuz"
-     wget -O $DIRECTORY/vmlinuz_7_32 "http://mirrors.rit.edu/centos/7/os/i386/images/pxeboot/vmlinuz"
-     wget -O $DIRECTORY/vmlinuz_7_64 "http://mirrors.rit.edu/centos/7/os/x86_64/images/pxeboot/vmlinuz"
-     return 0
-}
